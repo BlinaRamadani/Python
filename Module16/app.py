c48237d1182945db261695e28d9ce114b0cfb035
BlinaRamadani <blinaramadani739@gmail.com>
2026-05-16 11:13:58 +0200

    Module 16 and Module 17

--- Module16/app.py
+++ Module16/app.py
@@ -11,6 +11,21 @@ def main():
     st.write("You wrote: ", user_input)
     age = st.number_input("Enter your age",min_value=0, max_value=100)
     st.write(f"Your age is: {age}")
+    message = st.text_area("Enter a message")
+    st.write(f"Your message: {message}")
+    choice = st.radio("Pick one",["Choice 1","Choice 2", "Choice 3"])
+    st.write(f"You chose: {choice}")
+    if st.button("Success"):
+        st.success("Operation was successful")
+
+    try:
+        1/0
+    except Exception as e:
+        st.exception(e)
+
+
+
+
 
 if __name__ == "__main__":
     main()
